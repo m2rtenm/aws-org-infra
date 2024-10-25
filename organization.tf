75a558981d8b56bd53090b8442fed2cf6f2f6e5b
Märten Mändla <marten.mandla@gmail.com>
2024-10-26 00:09:32 +0300

    Role name fix

--- organization.tf
+++ organization.tf
@@ -32,26 +32,22 @@ resource "aws_organizations_account" "dev" {
   name      = "Development"
   email     = "marten.mandla+aws-dev@outlook.com"
   parent_id = aws_organizations_organizational_unit.non_prod.id
-  role_name = "dev-OrganizationAccountAccessRole"
 }
 
 resource "aws_organizations_account" "prod" {
   name      = "Production"
   email     = "marten.mandla+aws-prod@outlook.com"
   parent_id = aws_organizations_organizational_unit.prod.id
-  role_name = "prod-OrganizationAccountAccessRole"
 }
 
 resource "aws_organizations_account" "shared" {
   name      = "Shared services"
   email     = "marten.mandla+aws-shared@outlook.com"
   parent_id = aws_organizations_organizational_unit.other.id
-  role_name = "shared-OrganizationAccountAccessRole"
 }
 
 resource "aws_organizations_account" "sec" {
   name      = "Security and logging"
   email     = "marten.mandla+aws-sec@outlook.com"
   parent_id = aws_organizations_organizational_unit.other.id
-  role_name = "sec-OrganizationAccountAccessRole"
 }
